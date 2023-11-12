856c9f4499738749d9b55cb5cf50cba100371630
Amy <ampyaephyonaing@gmail.com>
2023-11-12 10:58:17 +0800

    Orchestrating Posts/Comments/Moderation/Query/Event Bus with K8s [ Deployment, Service ]

--- event-bus/index.js
+++ event-bus/index.js
@@ -12,24 +12,24 @@ app.post("/events", (req, res) => {
   
     events.push(event);
     // Post
-    axios.post("http://localhost:4000/events", event).catch((err) => {
+    axios.post("http://posts-clusterip-srv:4000/events", event).catch((err) => {
       console.log(err.message);
     });
 
-    //  Comment
-    axios.post("http://localhost:4001/events", event).catch((err) => {
-      console.log(err.message);
-    });
+    // //  Comment
+    // axios.post("http://localhost:4001/events", event).catch((err) => {
+    //   console.log(err.message);
+    // });
 
-    //  Query Service
-    axios.post("http://localhost:4002/events", event).catch((err) => {
-      console.log(err.message);
-    });
+    // //  Query Service
+    // axios.post("http://localhost:4002/events", event).catch((err) => {
+    //   console.log(err.message);
+    // });
 
-    // Moderation Service
-    axios.post("http://localhost:4003/events", event).catch((err) => {
-      console.log(err.message);
-    });
+    // // Moderation Service
+    // axios.post("http://localhost:4003/events", event).catch((err) => {
+    //   console.log(err.message);
+    // });
     res.send({ status: "OK" });
 });
 
